ac16615b898282743887919c71e5e1cf540a2283
usfbelhadj <usf.belhadj@gmail.com>
2020-11-06 18:01:42 +0100

    Del __init__

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -13,11 +13,8 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key and item is None:
-            pass
-        else:
+        if key and item is not None:
             self.cache_data[key] = item
-            return self.cache_data
 
     def get(self, key):
         """ Get an item by key
