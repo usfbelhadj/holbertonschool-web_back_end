0b803b0c8ef30f9443c01bb3d0f5748b83f7fd0f
usfbelhadj <usf.belhadj@gmail.com>
2020-11-06 14:52:57 +0100

    Test

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ BaseCaching module
 """
+from base_caching import BaseCaching
 
-
-class BasicCache():
+class BasicCache(BaseCaching):
     """ BaseCaching defines:
       - constants of your caching system
       - where your data are stored (in a dictionary)
@@ -11,16 +11,7 @@ class BasicCache():
     MAX_ITEMS = 4
 
     def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
+        BaseCaching.__init__(self)
 
     def put(self, key, item):
         """ Add an item in the cache
