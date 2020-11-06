18bf0f7eca830f5100bef9592aa7beb087bcbd92
usfbelhadj <usf.belhadj@gmail.com>
2020-11-06 18:07:47 +0100

    Update put fun

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -9,12 +9,13 @@ class BasicCache(BaseCaching):
       - constants of your caching system
       - where your data are stored (in a dictionary)
     """
-
+    
     def put(self, key, item):
         """ Add an item in the cache
         """
         if key and item is not None:
             self.cache_data[key] = item
+            return self.cache_data
 
     def get(self, key):
         """ Get an item by key
