5824ac3ca47e67eecbca43f8ead45a17364a85b1
usfbelhadj <usf.belhadj@gmail.com>
2020-11-07 12:31:33 +0100

    add Fifi_cache

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""FIFOCache module
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache definition:
+       FIFO algorithm
+    """
+
+    def __init__(self):
+        """Initiliaze
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        dicl = []
+        t = []
+        if key or item is not None:
+            self.cache_data[key] = item
+            for k, v in self.cache_data.items():
+                t = [k, v]
+                dicl.append(t)
+                if len(dicl) > BaseCaching.MAX_ITEMS:
+                    print("DISCARD: {}".format(dicl[0][0]))
+                    dicl.pop(0)
+                    self.cache_data = dict(dicl)
+        else:
+            return None
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key not in self.cache_data.keys():
+            return None
+        else:
+            return self.cache_data[key]
