cb937b45153928674eb55b90ed4fabf3c49ff8a1
usfbelhadj <usf.belhadj@gmail.com>
2021-03-15 19:00:49 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Basic annotations - concat mandatory
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    function concat that takes a string str1 and a string str2 as arguments/
+    and returns a concatenated string
+    '''
+    return str1 + str2
