c6681325dfa0b7ae022949546767464327049e71
usfbelhadj <usf.belhadj@gmail.com>
2021-03-16 15:38:12 +0100

    The basics of async and Readme

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,5 +7,11 @@ from random import uniform
 
 
 async def wait_random(max_delay: float = 10.0) -> float:
+    '''
+    Write an asynchronous coroutine that takes in an integer argument/
+    (max_delay, with a default value of 10) named wait_random that /
+    waits for a random delay between 0 and max_delay /
+    (included and float value) seconds and eventually returns it.
+    '''
     d = uniform(max_delay, 0)
     return d
