f452e9e8771171433eaae7ed49a9bff1ddc62896
usfbelhadj <usf.belhadj@gmail.com>
2021-03-17 21:35:43 +0100

    Add Generator from typing

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,8 +8,10 @@ import asyncio
 
 from random import uniform
 
+from typing import Generator
 
-async def async_generator():
+
+async def async_generator() -> Generator[float, None, None]:
     '''
     Write a coroutine called async_generator that takes no arguments.
     The coroutine will loop 10 times, each time asynchronously wait 1 second,/
