4dec7bd13b7edf46fea43621532e7a0897e6739a
usfbelhadj <usf.belhadj@gmail.com>
2021-03-22 16:16:22 +0100

    LIFOCache module

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -2,6 +2,7 @@
 """LIFOCache module
 """
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class LIFOCache(BaseCaching):
@@ -10,30 +11,27 @@ class LIFOCache(BaseCaching):
     """
 
     def __init__(self):
-        """Initiliaze
+        """[initialization]
         """
         super().__init__()
+        self.l_k = []
 
     def put(self, key, item):
-        """ Add an item in the cache
-            and delete the other
-            Return: Value
+        """[put]
+        Args:
+            key ([str]): [key]
+            item ([str]): [value to assign]
         """
-        dicl = []
-        t = []
-        if key or item is not None:
+        if key and item:
             self.cache_data[key] = item
-            for k, v in self.cache_data.items():
-                t = [k, v]
-                dicl.append(t)
-                dicl.reverse()
-                if len(dicl) > BaseCaching.MAX_ITEMS:
-                    print("DISCARD: {}".format(dicl[0][0]))
-                    dicl.pop(-1)
-                    self.cache_data = dict(dicl)
-            return self.cache_data
-        else:
-            return None
+            if key not in self.l_k:
+                self.l_k.append(key)
+            else:
+                self.l_k.append(self.l_k.pop(self.l_k.index(key)))
+            if len(self.l_k) > BaseCaching.MAX_ITEMS:
+                del_key = self.l_k.pop(-2)
+                print("DISCARD: {}".format(del_key))
+                del self.cache_data[del_key]
 
     def get(self, key):
         """ Get an item by key
