d22a62cabb49f000fd3f9d8e685d236f5c74fb5c
usfbelhadj <usf.belhadj@gmail.com>
2021-03-22 21:29:29 +0100

    Lfu

---
+++ 0x03-caching/100-lfu_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""LFUCache module
+"""
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """LFUCache definition:
+       LFUCache algorithm
+    """
+
+    def __init__(self):
+        """[initialization]
+        """
+        super().__init__()
+        self.l_k = []
+
+    def put(self, key, item):
+        """[put]
+        Args:
+            key ([str]): [key]
+            item ([str]): [value to assign]
+        """
+        if key and item:
+            self.cache_data[key] = item
+            if key not in self.l_k:
+                self.l_k.append(key)
+            else:
+                self.l_k.append(self.l_k.pop(self.l_k.index(key)))
+            if len(self.l_k) > BaseCaching.MAX_ITEMS:
+                del_key = self.l_k.pop(0)
+                del self.cache_data[del_key]
+                print("DISCARD: {}".format(del_key))
+
+    def get(self, key):
+        """ Get an item by key
+            Return: value or None
+        """
+        if key and self.cache_data.get(key):
+            self.l_k.append(self.l_k.pop(self.l_k.index(key)))
+            return self.cache_data.get(key)
+        else:
+            return None
