8b86d623d47220113a63acb014ec8b50b9e8aedc
usfbelhadj <usf.belhadj@gmail.com>
2021-05-10 22:18:50 +0100

    function called filter_datum that returns the log message obfuscated

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import re
 from typing import List
+
 """
 filtered_logger
 """
@@ -8,9 +9,10 @@ filtered_logger
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    '''
+    """
     function called filter_datum that returns the log message obfuscated
-    '''
+    """
+
     for field in fields:
         message = re.sub(r'{}=.+?{}'.format(field, separator),
                          '{}={}{}'.format(field, redaction, separator),
