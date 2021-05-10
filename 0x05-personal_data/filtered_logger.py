ad46cc522e1b2e6dd191e1e23e14830fcc104c1e
usfbelhadj <usf.belhadj@gmail.com>
2021-05-10 22:24:56 +0100

    add all

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -7,13 +7,16 @@ The function should use a regex to replace occurrences of certain field values
 """
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """
     function called filter_datum that returns the log message obfuscated
     """
     for field in fields:
-        message = re.sub(r'{}=.+?{}'.format(field, separator),
-                         '{}={}{}'.format(field, redaction, separator),
-                         message)
+        message = re.sub(
+            r"{}=.+?{}".format(field, separator),
+            "{}={}{}".format(field, redaction, separator),
+            message,
+        )
     return message
