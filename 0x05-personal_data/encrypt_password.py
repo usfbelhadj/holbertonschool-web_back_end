0fa8e2c947f9b1c03e268969175636ff15d798ea
usfbelhadj <usf.belhadj@gmail.com>
2021-05-11 01:14:41 +0100

    Encrypting passwords22

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -15,7 +15,6 @@ def hash_password(password: str) -> bytes:
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
-    is_valid function that expects 2 arguments
-    and returns a boolean
+    expects 2 arguments and returns a boolean
     """
-    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
+    return bcrypt.checkpw(b"password", hashed_password)
