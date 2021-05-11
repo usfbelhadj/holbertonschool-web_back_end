9a4f8239fb3ede8e48ae95a98869532bd64b4f1d
usfbelhadj <usf.belhadj@gmail.com>
2021-05-11 17:18:03 +0100

    Auth class

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -12,10 +12,20 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns False - path and excluded_paths will be used later,
+        now, you don’t need to take care of them
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """
+        returns None - request will be the Flask request object
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar("User"):
+        """
+        returns None - request will be the Flask request object
+        """
         return None
