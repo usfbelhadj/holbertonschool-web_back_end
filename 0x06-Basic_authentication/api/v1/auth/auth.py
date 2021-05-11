479ef691898dfc4098484918f7c0f1ed27b54a1d
usfbelhadj <usf.belhadj@gmail.com>
2021-05-11 17:13:07 +0100

    Auth class

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Auth API
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Auth Class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        return request
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        return request
