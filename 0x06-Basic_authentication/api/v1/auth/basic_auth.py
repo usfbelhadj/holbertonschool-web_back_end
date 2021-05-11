2402d18701a10dc4ae8e07876cf7fbdd4aec9258
usfbelhadj <usf.belhadj@gmail.com>
2021-05-12 00:44:38 +0100

    Basic - Base64 part

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,4 +12,16 @@ class BasicAuth(Auth):
     BasicAuth Class
     """
 
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Basic - Base64 part
+        """
+        if authorization_header is None:
+            return None
+        elif type(authorization_header) is not str:
+            print("Waht")
+            return None
+        elif authorization_header[0:6] != "Basic ":
+            return None
+        else:
+            return authorization_header[6:]
