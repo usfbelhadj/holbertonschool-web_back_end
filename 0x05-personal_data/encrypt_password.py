c15b9272993ef92540e408720403d8d6d0ac9a7c
usfbelhadj <usf.belhadj@gmail.com>
2021-05-11 01:03:00 +0100

    Encrypting passwords

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -7,4 +7,8 @@ from typing import ByteString
 
 
 def hash_password(password: str) -> ByteString:
+    """
+    function that expects one string
+    argument name password and returns a salted
+    """
     return bcrypt.hashpw(b"password", bcrypt.gensalt())
