7e8229c7af64824e585ef0b78b92954c295342f4
usfbelhadj <usf.belhadj@gmail.com>
2021-05-12 01:30:37 +0100

    returns the decoded value of a Base64 string

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ BasicAuth API
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -24,3 +25,21 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                               str) -> str:
+        '''
+        returns the decoded value of a Base64 string
+        '''
+
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            base64.b64encode(base64.b64decode(base64_authorization_header
+                                              )) == base64_authorization_header
+        except Exception:
+            return None
+        return base64.b64decode(base64_authorization_header).decode('utf-8')
