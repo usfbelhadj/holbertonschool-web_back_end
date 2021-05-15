6970aca6d45cf704eba18a8073c0a0432e2443d4
usfbelhadj <usf.belhadj@gmail.com>
2021-05-15 23:52:11 +0100

    Task5 Before request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -65,13 +65,12 @@ def before_request_func() -> str:
     ):
         return None
     request.current_user = auth.current_user(request)
-    if auth.authorization_header(request) is None:
-        return abort(401)
-    if auth.current_user(request) is None:
-        return abort(403)
+
     if auth.authorization_header(request) and auth.session_cookie(
             request) is None:
         return abort(401)
+    if auth.current_user(request) is None:
+        return abort(403)
 
 
 if __name__ == "__main__":
