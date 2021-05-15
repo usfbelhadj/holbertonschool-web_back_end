b01b6655d0b1d35455a428256a23b23cb5d96102
usfbelhadj <usf.belhadj@gmail.com>
2021-05-15 23:21:18 +0100

    Task4  Session cookie

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -43,3 +43,20 @@ class Auth:
         returns None - request will be the Flask request object
         """
         return None
+
+    def session_cookie(self, request=None):
+        """[session_cookie]
+
+        Return None if request is None
+        Return the value of the cookie named _my_session_id
+        from request -the name of the cookie must be defined
+        by the environment variable SESSION_NAME
+        You must use .get() built-in for accessing the
+        cookie in the request cookies dictionary
+        You must use the environment variable SESSION_NAME to
+        define the name of the cookie used for the Session ID
+        """
+        if request is None:
+            return None
+        session_id = request.cookies.get("_my_session_id")
+        return session_id
