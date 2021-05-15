a24ce5c8f4c45a7f43cc3e19ce4e9c8354c90b7d
usfbelhadj <usf.belhadj@gmail.com>
2021-05-15 23:06:02 +0100

    Task2 Create a session

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -13,6 +13,15 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """[create_session]
+
+        Generate a Session ID using uuid module and uuid4() like id in Base
+        Use this Session ID as key of the dictionary user_id_by_session_id -
+        the value for this key must be user_id
+        Return the Session ID
+        The same user_id can have multiple Session ID - indeed, the user_id is
+        the value in the dictionary user_id_by_session_id
+        """
         if user_id is None or type(user_id) is not str:
             return None
         Session_ID = str(uuid4())
