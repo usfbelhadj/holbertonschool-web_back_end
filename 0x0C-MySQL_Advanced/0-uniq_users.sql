436d35e71a3f0248084cde95882279f46df8319b
usfbelhadj <usf.belhadj@gmail.com>
2021-06-02 00:55:20 +0100

    Test

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,10 @@
-CREATE TABLE users
-(
-    id int,
-    email VARCHAR(255),
-    nanme VARCHAR(255),
-)
+-- creates a table users following these requirements
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
+
