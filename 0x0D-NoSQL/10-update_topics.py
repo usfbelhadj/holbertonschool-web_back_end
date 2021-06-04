5515e772d8e63b4a7e6d4148ab33f09527f98d57
usfbelhadj <usf.belhadj@gmail.com>
2021-06-05 00:47:17 +0100

    Update

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -8,4 +8,4 @@ def update_topics(mongo_collection, name, topics):
     """
     function that changes all topics of a school document based on the name
     """
-    mongo_collection.update_many({"name": name}, {"$set": {"topic": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
