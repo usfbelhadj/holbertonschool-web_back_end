36a501b567342737350397887db7911fc77d6a95
usfbelhadj <usf.belhadj@gmail.com>
2021-06-09 01:38:19 +0100

    Let's create a report object

---
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+export default function createReportObject(employeesList) {
+  return {
+
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(args) {
+      return Object.keys(args).length;
+    },
+  };
+}
