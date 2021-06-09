e6a814fec584c4c2b372cf7f8c0ee307f5146434
usfbelhadj <usf.belhadj@gmail.com>
2021-06-09 21:59:08 +0100

    Using Process stdin

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,14 +1,7 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
-});
-rl.on('close', () => {
-  console.log('This important software is now closing');
-  process.exit(0);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
