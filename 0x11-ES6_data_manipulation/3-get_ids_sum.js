a98799f437640835d9ef159783c6ceec3d8273c9
usfbelhadj <usf.belhadj@gmail.com>
2021-06-11 00:59:04 +0100

    Reduce

--- 0x11-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x11-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum(students) {
   const reducer = (accumulator, currentValue) => accumulator + currentValue.id;
-  return students.reduce(reducer);
+  return students.reduce(reducer, 0);
 }
