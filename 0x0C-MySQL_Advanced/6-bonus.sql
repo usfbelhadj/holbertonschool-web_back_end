f10158af02d0cf6788d45ea2554692c08d69ff74
usfbelhadj <usf.belhadj@gmail.com>
2021-06-12 21:08:14 +0100

    Test

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,11 +1,11 @@
 -- Write a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
 DELIMITER //
 
-CREATE PROCEDURE AddBonus(user_id INT, project_name varchar(255), score INT)
+CREATE PROCEDURE Addbonus(user_id INT, project_name VARCHAR(255), score INT)
 BEGIN
 IF EXISTS(SELECT * FROM projects WHERE name = project_name LIMIT 1)
 THEN SET @projectid = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
-    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, @projectid, score);
+INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, @projectid, score);
 ELSE 
 INSERT INTO projects (name) VALUES (project_name);
 END IF;
