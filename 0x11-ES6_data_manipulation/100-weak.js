c3c9174b79539069d9f830381dff217e8d9688b5
usfbelhadj <usf.belhadj@gmail.com>
2021-06-13 02:21:09 +0100

    Weak link data structure

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -2,9 +2,10 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 1);
-  }
-  weakMap.set(endpoint, 1 + weakMap.get(endpoint));
-  if (weakMap.get(endpoint) >= 5) {
-    throw (Error('Endpoint load is high'));
+  } else {
+    weakMap.set(endpoint, 1 + weakMap.get(endpoint));
+    if (weakMap.get(endpoint) >= 5) {
+      throw (Error('Endpoint load is high'));
+    }
   }
 }
